1af89edb1fe28130cff9769f3d3465bfcb8be8a7
Thant Ko Zaw <thantmkou10@gmail.com>
2021-08-24 13:14:33 +0630

    Display board

--- tic_tac_toe.rb
+++ tic_tac_toe.rb
@@ -1,9 +1,36 @@
+class Board
+  def initialize
+    @squares = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+  end
+  
+  def display
+    @squares.each do |square|
+      print "\n" if square % 3 == 1
+      print " #{square} |"
+    end
+  end
+
+  def self.update(name, number)
+    # TODO
+  end
+
+  def self.result
+    # TODO
+  end
+end
+
 class Player
   def initialize(name, symbol)
     @name = name
     @symbol = symbol
   end
+
+  def play(choice)
+    # TODO
+  end
 end
 
-p1 = Player.new("Marco", "X")
-p1 = Player.new("Aaron", "O")
+# Execute method is needed here
+board = Board.new
+board.display
+print "\n"
