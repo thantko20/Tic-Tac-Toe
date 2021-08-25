c6b505fbe0dd84b39c83150f57b3e3ffb85ca4fd
Thant Ko Zaw <thantmkou10@gmail.com>
2021-08-25 20:37:28 +0630

    Add how to play instructions and comments

--- tic_tac_toe.rb
+++ tic_tac_toe.rb
@@ -1,11 +1,17 @@
-require 'pry-byebug'
+# frozen_string_literal: true
 
+# Instructions
+# Simple 3x3 grid
+# Two human players
+# Announce Win or lose or draw
+
+# This module will be used in result checking
 module Result
-  WIN_PATTERNS = [[0,1,2], [3,4,5], [6,7,8], [0,3,6], [1,4,7], [2,5,8], [0,4,8], [2,4,6]]
-  # win_patterns = 
+  WIN_PATTERNS = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]].freeze
+
   def win1?
     win = WIN_PATTERNS.each do |pattern|
-      if @grid[pattern[0]] == @p1.symbol and @grid[pattern[1]] == @p1.symbol and @grid[pattern[2]] == @p1.symbol
+      if (@grid[pattern[0]] == @p1.symbol) && (@grid[pattern[1]] == @p1.symbol) && (@grid[pattern[2]] == @p1.symbol)
         return true
       end
     end
@@ -14,7 +20,7 @@ module Result
 
   def win2?
     win = WIN_PATTERNS.each do |pattern|
-      if @grid[pattern[0]] == @p2.symbol and @grid[pattern[1]] == @p2.symbol and @grid[pattern[2]] == @p2.symbol
+      if (@grid[pattern[0]] == @p2.symbol) && (@grid[pattern[1]] == @p2.symbol) && (@grid[pattern[2]] == @p2.symbol)
         return true
       end
     end
@@ -28,52 +34,56 @@ end
 
 class Player
   attr_reader :name, :symbol
+
   def initialize(name, symbol)
     @name = name
     @symbol = symbol
   end
 end
 
+# This board operates all the things happening underneath and 'mixin' with Result module to check result
 class Board
   include Result
   def initialize(p1, p2)
     @p1 = p1
     @p2 = p2
     @grid = [' ', ' ', ' ',
-            ' ', ' ', ' ', 
-            ' ', ' ', ' ']
+             ' ', ' ', ' ',
+             ' ', ' ', ' ']
     @turn = -1
   end
 
   def display
     puts "#{@grid[0]} | #{@grid[1]} | #{@grid[2]}"
-    puts "---------"
+    puts '--+---+--'
     puts "#{@grid[3]} | #{@grid[4]} | #{@grid[5]}"
-    puts "---------"
+    puts '--+---+--'
     puts "#{@grid[6]} | #{@grid[7]} | #{@grid[8]}"
+    puts ''
   end
 
   def play
-    until win1? or win2? or draw?
-      #binding.pry
-      puts "Cell: "
+    until win1? || win2? || draw?
+      puts 'Cell: '
       cell = gets.chomp.to_i - 1
-      unless self.valid_input?(cell)
-        puts "Invalid input!"
+      puts ''
+
+      unless valid_input?(cell)
+        puts 'Invalid input!'
         next
       end
-      @grid[cell] = self.switch
-      self.display
+
+      @grid[cell] = switch
+      display
     end
-    self.final_result
+    final_result
   end
 
   private
 
   def valid_input?(input)
-    if !input.between?(0, 8) or @grid[input] == @p1.symbol or @grid[input] == @p2.symbol
-      return false
-    end
+    return false if !input.between?(0, 8) || (@grid[input] == @p1.symbol) || (@grid[input] == @p2.symbol)
+
     true
   end
 
@@ -93,8 +103,33 @@ class Board
   end
 end
 
-p1 = Player.new('Marco', 'X')
-p2 = Player.new('Aaron', 'O')
-game = Board.new(p1, p2)
-game.display
-game.play
+def tic_tac_toe
+  puts "Welcome! This is Tic-Tac-Toe PvP. Rules are simple.\n
+If you can get your three symbols in consecutive order(x-axis or y-axis or diagonally), you win!\n
+Here's the cells positions."
+
+  puts '1 | 2 | 3'
+  puts '--+---+--'
+  puts '4 | 5 | 6'
+  puts '--+---+--'
+  puts '7 | 8 | 9'
+  puts ''
+
+  puts 'Enter player 1 name: '
+  p1_name = gets.chomp.to_s
+  puts 'Enter player 1 symbol: '
+  p1_symbol = gets.chomp.to_s
+
+  puts 'Enter player 2 name: '
+  p2_name = gets.chomp.to_s
+  puts 'Enter player 2 symbol: '
+  p2_symbol = gets.chomp.to_s
+
+  p1 = Player.new(p1_name, p1_symbol)
+  p2 = Player.new(p2_name, p2_symbol)
+  game = Board.new(p1, p2)
+  game.display
+  game.play
+end
+
+tic_tac_toe
