116c20c4c6906f5f8c6889211d78f64c52e5bcc7
Thant Ko Zaw <thantmkou10@gmail.com>
2021-08-25 19:33:54 +0630

    Add validating input logic

--- tic_tac_toe.rb
+++ tic_tac_toe.rb
@@ -1,3 +1,5 @@
+require 'pry-byebug'
+
 module Result
   WIN_PATTERNS = [[0,1,2], [3,4,5], [6,7,8], [0,3,6], [1,4,7], [2,5,8], [0,4,8], [2,4,6]]
   # win_patterns = 
@@ -37,9 +39,9 @@ class Board
   def initialize(p1, p2)
     @p1 = p1
     @p2 = p2
-    @grid = [' ', ' ', ' ',
-             ' ', ' ', ' ', 
-             ' ', ' ', ' ']
+    @grid = [1, 2, 3,
+             4, 5, 6, 
+             7, 8, 9]
     @turn = -1
   end
 
@@ -53,15 +55,27 @@ class Board
 
   def play
     until win1? or win2? or draw
+      #binding.pry
       puts "Cell: "
-      cell = gets.chomp.to_i
-      @grid[cell-1] = self.switch
+      cell = gets.chomp.to_i - 1
+      unless self.valid_input?(cell)
+        puts "Invalid input!"
+        next
+      end
+      @grid[cell] = self.switch
       self.display
     end
     self.final_result
   end
 
   private
+
+  def valid_input?(input)
+    if !input.between?(0, 8) or @grid[input] == @p1.symbol or @grid[input] == @p2.symbol
+      return false
+    end
+    true
+  end
 
   def switch
     @turn += 1
